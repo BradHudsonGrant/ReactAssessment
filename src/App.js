5cc89d8eeaa44957038e49fb1a61c81412485469
Sam Turnbull <oli.loades@gmail.com>
2018-10-26 16:01:58 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+
+
+export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+        sentence : ""
+    }
+
+    this.addToDisplay = (digit) => this.setState({
+        sentence : this.state.sentence + digit.target.value
+    }); 
+
+}
+
+  calculateOperations(){
+    var result = document.getElementById('result');
+    this.setState({
+      sentence: Math.eval(result)
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>            
+          <input id="result"  value={this.state.sentence} readonly></input>
+            <div id="main">
+                <div>
+                <button value="1" onClick ={this.addToDisplay}>1</button>
+                <button value="2" onClick ={this.addToDisplay}>2</button>
+                <button value="3" onClick ={this.addToDisplay}>3</button>
+                <button value="+" onClick ={this.addToDisplay}>+</button>
+                </div>
+                <div>
+                <button value="4" onClick ={this.addToDisplay}>4</button>
+                <button value="5" onClick ={this.addToDisplay}>5</button>
+                <button value="6" onClick ={this.addToDisplay}>6</button>
+                <button value="-" onClick ={this.addToDisplay}>-</button>
+                </div>
+
+                <div>
+                <button value="7" onClick ={this.addToDisplay}>7</button>
+                <button value="8" onClick ={this.addToDisplay}>8</button>
+                <button value="9" onClick ={this.addToDisplay}>9</button>
+                <button value="/" onClick ={this.addToDisplay}>/</button>
+                </div>
+                    
+                <div>
+                <button value="0" onClick ={this.addToDisplay}>0</button>
+                <button value="=" onClick ={this.addToDisplay}>=</button>
+                <button value="/" onClick ={this.calculateOperations}>/</button>
+                </div>
+              </div>
+          </div>  
     );
   }
 }
-
-export default App;
